cee3aa179f6d895df42420bd56983535b8f2d6b3
VictorD <vdnavaa01@larioja.edu.es>
2024-03-29 19:43:44 +0100

    implementacion test constructor con parametros

--- src/test/java/instituto/PersonaTest.java
+++ src/test/java/instituto/PersonaTest.java
@@ -2,6 +2,8 @@ package instituto;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonaTest {
@@ -20,5 +22,17 @@ class PersonaTest {
         assertEquals(' ', persona.getGenero());
         assertNotNull(persona.getNif());
     }
+
+    @Test
+    public void testConstructorParametros() {
+        Persona persona2 = new Persona(12345678,"Pepe",'H',12,5,1995);
+
+        assertEquals("Pepe", persona2.getNombre());
+        assertEquals('H', persona2.getGenero());
+        assertEquals(LocalDate.of(1995,5,12),
+                persona2.getNacimiento());
+
+    }
+
 
 }
